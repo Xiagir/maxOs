cd030bdd275916400c9ba9a605759ebb0d88b966
Xiagir <41594336+Xiagir@users.noreply.github.com>
2019-11-06 18:35:53 -0500

    Update steam.sh

--- steam.sh
+++ steam.sh
@@ -2,4 +2,4 @@
 curl https://steamcdn-a.akamaihd.net/client/installer/steam.dmg -o ~/Desktop/steam.dmg;
 hdiutil attach ~/Desktop/steam.dmg;
 cp -r /Volumes/Steam/ ~/Desktop/;
-./Steam.app/Contents/MacOS/steam_osx
+~/Desktop/Steam.app/Contents/MacOS/steam_osx
