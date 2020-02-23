ff026fa3aafd71e28723e2371895b1f6582badc0
Xiagir <41594336+Xiagir@users.noreply.github.com>
2020-02-23 16:09:06 -0500

    Update unity.sh

--- unity.sh
+++ unity.sh
@@ -3,6 +3,7 @@ curl "https://beta.unity3d.com/download/1f86fad89a55/MacEditorInstaller/Unity.pk
 curl "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.dmg" -o ~/Desktop/hub.dmg
 hdiutil attach ~/Desktop/hub.dmg
 cp -R /Volumes/Unity\ Hub\ 2.2.2/ ~/Desktop/
+mkdir ~/Unity
 mv ~/Desktop/unity.pkg ~/Unity
 pkgutil --expand ~/Unity/unity.pkg ~/Unity/Installer
 tar xvf ~/Unity/Installer/Unity.pkg.tmp/Payload -C ~/Unity
